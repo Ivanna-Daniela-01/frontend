204603e32aa6b86e75f67a44924a9086b8be60a7
Ivanna Cevallos <ivanna.cevallos@epn.edu.ec>
2024-04-19 14:55:19 -0500

    CREATION COMPONENT home, CREATION SERVICE AND MODEL FOR person

---
+++ my-frontend-app/src/app/models/person.ts
@@ -0,0 +1,10 @@
+export class Person {
+    constructor(
+      public _id: string,
+      public name: string,
+      public lastname: string,
+      public mail: string,
+      public password: string
+    ) {}
+  }
+  
