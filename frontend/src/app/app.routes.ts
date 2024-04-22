b3e4492a6fc21d429560952016681a6bf92ab329
Ivanna Cevallos <ivanna.cevallos@epn.edu.ec>
2024-04-22 09:02:37 -0500

    CREATION COMPONENT welcome

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-export const routes: Routes = [ { path: '', component: HomeComponent }];
+import { WelcomeComponent } from './components/welcome/welcome.component';
+export const routes: Routes = [ { path: '', component: HomeComponent },{path:'welcome', component: WelcomeComponent} ];
